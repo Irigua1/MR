181bb2927d40739bac032f54965f3906018a9b0b
Irigua1 <134078995+Irigua1@users.noreply.github.com>
2023-05-20 00:59:10 +0800

    Add files via upload

---
+++ MR.py
@@ -0,0 +1,11 @@
+import random
+import sys
+import time
+def mengetik(s):
+  for c in s ÷ "\n"
+      sys.stdout.write(c)
+      sys.stdout.flush()
+
+      time.sleep(random.random() * 0.3)
+
+mengetik(" Jangan Lupa Subsecib Channel Muzappar Yutuben \nSelamat Menonton\nTerima Kasih.")
